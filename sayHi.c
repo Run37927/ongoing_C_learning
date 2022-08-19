89edb20fbfeedfcf3f042c37504677c0a09b0ea9
Hairun Huang <hairunhuang@gmail.com>
2022-08-18 19:44:31 -0700

    say hi

---
+++ sayHi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <limits.h>
+
+
+int main()
+{
+    // unsigned int var1 = 0;
+    // unsigned int var2 = UINT_MAX;
+
+    // printf("The range of unsigned integer is from: %u to %u\n", var1, var2);
+    // printf("size of signed long long int is %ld\n", sizeof(long long int));
+    // printf("size of unsigned long long int is %lu\n", sizeof(long long int));
+  
+
+    int i = 0;
+    for (printf("one\n"); i<3 && printf(""); i++) {
+        printf("Hi!\n");
+    }
+    
+    return 0;
+}
