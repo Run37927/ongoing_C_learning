64b93e08bf86bbe8cea5ad28459f1a4b02d2c384
Hairun Huang <hairunhuang@gmail.com>
2022-08-18 19:40:51 -0700

    half adder

---
+++ halfAdder.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum, carry, a, b;
+    printf("enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (b !=0) {
+        sum = a^b;
+        carry = (a&b) << 1;
+        a = sum;
+        b = carry;
+    }
+
+    printf("sum is %d", sum);
+
+    return 0;
+}
