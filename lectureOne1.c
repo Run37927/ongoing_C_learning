8147e704721e51ff0256f8ac3d3af4a8a3fa8baf
Hairun Huang <hairunhuang@gmail.com>
2022-09-12 17:27:17 -0700

    123

--- lectureOne1.c
+++ lectureOne1.c
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
     int n, m;
     n = atoi(argv[1]);
     m = atoi(argv[2]);
-    printf("argc: %d\n", argc);
+    printf("argc: &d\n", argc);
     printf("argument 1: %d\nargument 2: %d\n", n, m);
 
     return 0;
