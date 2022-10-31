16e15db168f282d3bf2c643c05e83398c885c09a
Hairun Huang <hairunhuang@gmail.com>
2022-10-31 08:27:13 -0700

    Update insertNode.c

--- insertNode.c
+++ insertNode.c
@@ -44,18 +44,20 @@ int main()
     head->data = 45;
     head->link = NULL;
 
-    struct node *current = (struct node *)malloc(sizeof(struct node));
-    current->data = 98;
-    current->link = NULL;
-    head->link = current;
+    // struct node *current = (struct node *)malloc(sizeof(struct node));
+    // current->data = 98;
+    // current->link = NULL;
+    // head->link = current;
 
-    current = (struct node *)malloc(sizeof(struct node));
-    current->data = 3;
-    current->link = NULL;
+    // current = (struct node *)malloc(sizeof(struct node));
+    // current->data = 3;
+    // current->link = NULL;
 
-    head->link->link = current;
+    // head->link->link = current;
 
-    insertAtEnd(head, 67);
+    for (int i=5; i<10; i++) {
+        insertAtEnd(head, i);
+    }
     printData(head);
     return 0;
 }
