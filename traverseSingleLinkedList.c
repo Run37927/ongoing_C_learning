1c739b16c175d1a0654e659d75436db6515d76b8
Hairun Huang <hairunhuang@gmail.com>
2022-10-30 23:38:59 -0700

    traverse linked list

--- traverseSingleLinkedList.c
+++ traverseSingleLinkedList.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-// suppose we want to store a list of numbers 23 54 78 90
-// can use an array
-// or use a linked list
+
+// traverse a single linked list to the end and count how many nodes are there
+
+// suppose 45 -> 98 -> 3
 
 struct node {
     int data;
     struct node *link;
 };
 
+void countNodes (struct node *head)
+{
+    int count = 0;
+    if (head == NULL)
+        printf("linked list is empty");
+    
+    struct node *ptr = NULL;
+    ptr = head;
+    while (ptr != NULL) {
+        count ++;
+        ptr = ptr->link;
+    }
+    printf("%d\n", count);
+}
+
+
 int main()
 {
-    int arr[] = {23, 54, 78, 90};
-
     struct node *head = (struct node *)malloc(sizeof(struct node));
-    head->data = 23;
+    head->data = 45;
     head->link = NULL;
 
     struct node *current = (struct node *)malloc(sizeof(struct node));
     current->data = 54;
     current->link = NULL;
-
-    // to link up the first node
-    // first node is connected with second node now
     head->link = current;
 
     current = (struct node *)malloc(sizeof(struct node));
@@ -31,5 +43,5 @@ int main()
 
     head->link->link = current;
 
-    return 0;
+    countNodes(head);
 }
