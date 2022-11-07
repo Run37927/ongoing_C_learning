d43b9e56a59f6328b712077d293eefc9ea8b22f8
Hairun Huang <hairunhuang@gmail.com>
2022-11-07 10:07:27 -0800

    Create deleteEntireSingleLinkedList.c

---
+++ deleteEntireSingleLinkedList.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// struct node* del_entire_list(struct node *head) {
+//     struct node *temp = head;
+//     while (temp != NULL) {
+//         temp = temp->link;
+//         free(head);
+//         head = temp;
+//     }
+//     return head;
+// }
