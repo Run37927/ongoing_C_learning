19cda6f6f288429f5b1c1d3edba20fed32b69ede
Hairun Huang <hairunhuang@gmail.com>
2022-11-18 08:46:23 -0800

    insert at beginning

--- createDoublyLinkedList.c
+++ createDoublyLinkedList.c
@@ -7,8 +7,6 @@ struct node {
     struct node *next;
 };
 
-
-
 struct node * addToEmpty(struct node *head, int data)
 {
     struct node *temp = malloc(sizeof(struct node));
@@ -19,12 +17,37 @@ struct node * addToEmpty(struct node *head, int data)
     return head;
 }
 
+struct node * addAtBeginning(struct node *head, int data)
+{
+    // receive two arguments: head and data
+    // receiving head means receiving the node
+
+    struct node *temp = malloc(sizeof(struct node)); // create a new node
+    temp->prev = NULL;
+    temp->data = data;
+    temp->next = NULL;
+
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+
+    return head;
+}
+
 int main()
 {
     struct node *head = NULL;
-    head = addToEmpty(head, 45); // insert into an empty list
-    printf("%d ", head->data);
+    struct node *ptr;
 
+    head = addToEmpty(head, 45); // insert into an empty list
+    head = addAtBeginning(head, 34); // insert at beginning of the list
+
+    ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
 
     return 0;
 }
